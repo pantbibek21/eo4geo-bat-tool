d1fca4601437abcec50ac5418912ed1993a59b72
Bibek Pant <pantbibek21@gmail.com>
2025-03-09 18:01:27 +0100

    refactor: remove the BoK Component and add in annotateDocument component

--- src/app/components/mainPage/mainPage.component.ts
+++ src/app/components/mainPage/mainPage.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { BokComponent } from '@eo4geo/ngx-bok-visualization';
 import { IntroductionComponent } from '../introduction/introduction.component';
 import { UploadDocumentComponent } from '../upload-document/upload-document.component';
 import { AnnotateDocumentComponent } from '../annotate-document/annotate-document.component';
@@ -10,7 +9,6 @@ import { AnnotateDocumentComponent } from '../annotate-document/annotate-documen
   templateUrl: './mainPage.component.html',
   styleUrls: ['./mainPage.component.css'],
   imports: [
-    BokComponent,
     IntroductionComponent,
     UploadDocumentComponent,
     AnnotateDocumentComponent,
