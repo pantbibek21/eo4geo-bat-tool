0f3cd9daec29156145ec4f0abfe15586ecba05b9
Bibek Pant <pantbibek21@gmail.com>
2025-03-09 11:57:13 +0100

    feat: lists the available BoK concepts in pdf

--- src/app/components/annotate-document/annotate-document.component.ts
+++ src/app/components/annotate-document/annotate-document.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-annotate-document',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './annotate-document.component.html',
   styleUrl: './annotate-document.component.css',
 })
 export class AnnotateDocumentComponent {
-  bokConcepts: string = '';
+  bokConcepts: string[] = [];
 
   constructor(private sharedService: SharedService) {}
 
@@ -17,8 +18,14 @@ export class AnnotateDocumentComponent {
   }
 
   ngOnInit() {
-    this.sharedService.bokConcept$.subscribe((value) => {
-      this.bokConcepts = value; // Updates when value changes
+    this.sharedService.bokConcept$.subscribe((bok) => {
+      if (bok) {
+        this.bokConcepts = [
+          ...[...bok.matchAll(/eo4geo:([A-Z]+[\d\-]+)/g)].map(
+            (match) => match[1]
+          ),
+        ];
+      }
     });
   }
 }
