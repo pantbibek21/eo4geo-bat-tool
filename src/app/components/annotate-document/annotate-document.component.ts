01569f5aca33c6b41d797f24bdf29b6e8f8ee164
Bibek Pant <pantbibek21@gmail.com>
2025-03-09 18:02:35 +0100

    feat: add colors, event listeners to delete to listed BoK concepts

--- src/app/components/annotate-document/annotate-document.component.ts
+++ src/app/components/annotate-document/annotate-document.component.ts
@@ -1,31 +1,77 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedService } from '../../services/shared.service';
+import {
+  BokComponent,
+  BokInformationService,
+} from '@eo4geo/ngx-bok-visualization';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-annotate-document',
-  imports: [CommonModule],
+  imports: [CommonModule, BokComponent],
   templateUrl: './annotate-document.component.html',
   styleUrl: './annotate-document.component.css',
 })
 export class AnnotateDocumentComponent {
   bokConcepts: string[] = [];
+  concept: string = 'GIST';
+  conceptName: string = '';
+  conceptColor: string = '';
+  message: string = '';
 
-  constructor(private sharedService: SharedService) {}
+  constructor(
+    private sharedService: SharedService,
+    private bokInfoService: BokInformationService
+  ) {}
 
   onClear() {
-    this.sharedService.triggerClear();
+    this.bokConcepts = [];
+    // this.sharedService.triggerClear();
+  }
+
+  deleteBokConcept(concept: string) {
+    this.bokConcepts = this.bokConcepts.filter((item) => item !== concept);
+  }
+
+  addAnnotation() {
+    if (this.bokConcepts.includes(this.concept)) {
+      this.message = 'Concept already included!';
+
+      setTimeout(() => (this.message = ''), 3000);
+    } else {
+      // fetch the color
+
+      this.bokConcepts.push(this.concept);
+    }
+  }
+
+  getBackgroundColor(concept: string): Observable<string> {
+    return this.bokInfoService.getConceptColor(concept);
+  }
+
+  getConceptName(concept: string) {
+    this.bokInfoService.getConceptName(concept).subscribe((name) => {
+      this.conceptName = name;
+    });
+
+    return this.conceptName;
   }
 
   ngOnInit() {
+    //  Listen for bokConcept updates
     this.sharedService.bokConcept$.subscribe((bok) => {
-      if (bok) {
-        this.bokConcepts = [
-          ...[...bok.matchAll(/eo4geo:([A-Z]+[\d\-]+)/g)].map(
-            (match) => match[1]
-          ),
-        ];
+      if (typeof bok === 'string') {
+        // Ensure it's a string
+        this.bokConcepts = [...bok.matchAll(/eo4geo:([\w\d-]+)/g)].map(
+          (match) => match[1]
+        );
       }
+    });
+
+    // Get concept name
+    this.bokInfoService.getConceptName('GIST').subscribe((name) => {
+      this.conceptName = name;
     });
   }
 }
