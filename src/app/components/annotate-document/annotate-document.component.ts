2e3a6421d659af8f53673553bac70edd01c980bf
Bibek Pant <pantbibek21@gmail.com>
2025-04-11 08:45:07 +0200

    refactor: implement BoK methods via shared service

--- src/app/components/annotate-document/annotate-document.component.ts
+++ src/app/components/annotate-document/annotate-document.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BokInformationService } from '@eo4geo/ngx-bok-visualization';
 import { map, Observable, Subscription, take } from 'rxjs';
 import { FileService } from '../../services/file.service';
 import { ButtonModule } from 'primeng/button';
 import { ChipModule } from 'primeng/chip';
 import { TooltipModule } from 'primeng/tooltip';
 import { ThemeUtils } from '@primeng/themes';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-annotate-document',
@@ -27,7 +27,7 @@ export class AnnotateDocumentComponent implements OnInit, OnDestroy {
 
   constructor(
     private fileService: FileService,
-    private bokInfoService: BokInformationService
+    private sharedService: SharedService
   ) {}
 
   ngOnInit() {
@@ -76,39 +76,12 @@ export class AnnotateDocumentComponent implements OnInit, OnDestroy {
   }
 
   getBackgroundColor(concept: string): Observable<string> {
-    return this.bokInfoService.getConceptColor(concept).pipe(
-      take(1),
-      map((hex) => this.hexToRgba(hex, 0.5))
-    );
+    return this.sharedService.getBackgroundColor(concept);
   }
 
   // fetches the BoK keyword title
   getConceptName(concept: string) {
-    return this.bokInfoService.getConceptName(concept).pipe(
-      take(1),
-      map((name) => name)
-    );
-  }
-
-  // makes the BoK tags light adding opacity
-  private hexToRgba(hex: string, alpha: number): string {
-    // Remove the hash if it exists
-    hex = hex.replace(/^#/, '');
-
-    // Parse r, g, b values
-    let r: number, g: number, b: number;
-    if (hex.length === 3) {
-      // Convert shorthand hex (e.g., #abc to #aabbcc)
-      r = parseInt(hex[0] + hex[0], 16);
-      g = parseInt(hex[1] + hex[1], 16);
-      b = parseInt(hex[2] + hex[2], 16);
-    } else {
-      r = parseInt(hex.substring(0, 2), 16);
-      g = parseInt(hex.substring(2, 4), 16);
-      b = parseInt(hex.substring(4, 6), 16);
-    }
-
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    return this.sharedService.getConceptName(concept);
   }
 
   renderModal() {
